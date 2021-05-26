46aff11779108af43d888dce703f36281345230f
Cristian Arean <carean@fi.uba.ar>
2021-05-25 22:18:55 -0300

    main mas completo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from random import *
-from sudoku import crear_juego
+from random import randint
 from mapas import *
 from sudoku import *
 
@@ -10,9 +9,32 @@ def draw_sudoku(sudoku):
         for j in range(ANCHO_TABLERO):
             print(sudoku[i][j], end = ' ')
         print('\n')
-draw_sudoku(sudoku)
+
+def variables_a_insertar(): #tiene los if despues de cada variable para que sea mas facil saber que numero estuvo mal
+        while True:
+            valor = int(input('ingrese el valor a insertar[entre 0 y 9]: '))
+            if not valor in range(0,10):
+                return False
+            fila = int(input('indique en la fila que se va a inserta[entre 0 y 9]: '))
+            if not fila in range(0,10):
+                return False
+            columna = int(input('Indique en la columna que se va a insertar el valor[entre 0 y 9]: '))
+            if not columna in range(0,10):
+                return False
+            return True
+        return valor, fila, columna
 
 
+print(variables_a_insertar())
+
+
+
+
+'''
+while esta_terminado != True:
+    draw_sudoku(sudoku)
+pass
+'''
 '''
 #hago random y el numero se lo doy a una variable
 p = [randint in range (len(MAPAS))]
@@ -22,11 +44,13 @@ running = True
 While running == True:
     draw_sudoku
     pido input fila, columna y valor
+    chekear si es movimiento valido
+        si lo es insertar valor
+        si no borrar valor
     if fila or columna or valor == exit
         break
     sudoku = insertar valor si valor es cero borrar valor
-    
+
     chequear esta terminado
 
-'''    
-
+'''
